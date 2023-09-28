e9a4a74c96f2997e7165121d212e7f07d6575bac
Adoum15 <38683857+Adoum15@users.noreply.github.com>
2023-09-28 02:52:41 +0200

    drapeau du monde

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    
+    <div className='logo'>
+      <img src="./Tchad1.png" alt="logo react" />
+      <h3>Drapeau</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo/>
+      <Navigation/>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem qui
+        perspiciatis sapiente! Earum vitae, aperiam ad odit blanditiis, quod
+        vero molestias quibusdam repellat nisi numquam aliquam, voluptatum
+        accusantium beatae minus quasi. Pariatur mollitia molestias minima
+        quibusdam, odio voluptas debitis rerum nulla consectetur, ipsa ea, ut
+        alias modi sequi necessitatibus a voluptate autem optio dolore? Laborum
+        cupiditate voluptate omnis, nesciunt, magnam, dolorum atque ut dolorem
+        perferendis laboriosam ex dignissimos velit vel eum eaque? Nam ipsum
+        dicta, totam ad aliquid consequuntur quaerat similique minima ut
+        reprehenderit sed, id explicabo, maxime facilis. Delectus, doloremque?
+        Error dicta suscipit inventore nemo esse mollitia sapiente maiores?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur id,
+        eum repellendus illo hic cum consequatur maxime nobis quidem voluptatum,
+        possimus assumenda. Modi maxime tempora molestias optio officia libero
+        natus itaque. Nihil accusantium corporis nam blanditiis fugit laudantium
+        eos fugiat debitis quam? Incidunt magni ullam iste, dolorem molestiae
+        architecto tempora facilis minima ipsam quidem voluptate cum
+        perspiciatis excepturi quisquam ab tempore voluptatem in asperiores ea.
+        Unde quia quo numquam! Distinctio commodi sapiente dolores minima fugit
+        atque omnis doloribus voluptatibus dolor natus doloremque ratione
+        quaerat expedita animi, obcaecati esse eveniet qui maiores debitis
+        tenetur. Quaerat amet doloremque dolores accusantium deleniti.
+        Praesentium?
+      </p>
+    </div>
+  );
+};
+
+export default About;
